c1266a99345f595a027ce00f38d101d4c45aa3d1
chadh <chadh>
2019-03-04 20:45:55 +0000

    add outputs

--- test-terraform-project/outputs.tf
+++ test-terraform-project/outputs.tf
@@ -5,10 +5,10 @@
 #####################################################################
 
 output "vm-1-public_ip" {
-  value = "${aws_instance.vm-1.public_ip}"
+  value = "${aws_instance.vm-1.private_ip}"
 }
 
 output "vm-2-public-ip" {
-  value = "${aws_instance.vm-2.public_ip}"
+  value = "${aws_instance.vm-2.private_ip}"
 }
 
